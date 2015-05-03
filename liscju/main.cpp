6388dc8bb8b59f3c35fc7458d002571cb7fec5d3
liscju <piotr.listkiewicz@gmail.com>
2015-05-03 15:35:53 +0200

    Added command line arguments handling

--- liscju/main.cpp
+++ liscju/main.cpp
@@ -4,68 +4,41 @@
 #include <vector>
 #include <omp.h>
 
-int bucketMain();
-
 void initialize_cmd_arguments(int argc, char **argv);
+void bucket_sort_init(int argc,char **argv);
+void usage(char *program_name);
 
 using namespace std;
-/*
-// Function to sort arr[] of size n using bucket sort
-void bucketSort(float arr[], int n)
-{
-	// 1) Create n empty buckets
-	vector<float> b[n];
 
-	// 2) Put array elements in different buckets
-	for (int i=0; i<n; i++)
-	{
-		int bi = n*arr[i]; // Index in bucket
-		b[bi].push_back(arr[i]);
-	}
+int size_of_array;
 
-	// 3) Sort individual buckets
-	for (int i=0; i<n; i++)
-		sort(b[i].begin(), b[i].end());
-
-	// 4) Concatenate all buckets into arr[]
-	int index = 0;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < b[i].size(); j++)
-			arr[index++] = b[i][j];
-}
-
-int bucketSortMain() {
-	float arr[] = {0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	bucketSort(arr, n);
-
-	cout << "Sorted array is \n";
-	for (int i=0; i<n; i++)
-		cout << arr[i] << " ";
-	return 0;
-}*/
-
-void usage() {
-	printf("./a.out");
+void usage(char *program_name) {
+	printf("Usage:\n ./%s size_of_array",program_name);
 	exit(0);
 }
 
 void initialize_cmd_arguments(int argc, char **argv) {
+	if (argc == 1)
+		usage(argv[0]);
 
+	size_of_array = atoi(argv[1]);
 }
 
 void bucket_sort_init(int argc,char **argv) {
 	initialize_cmd_arguments(argc,argv);
+	printf("Size of array = %d",size_of_array);
 }
 
 
 /* Driver program to test above funtion */
 int main(int argc,char **argv) {
-
+/*
 	omp_set_num_threads(3);
 	#pragma omp parallel for
 	for (int i=0;i<10;i++) {
 		printf("Thread id: %d iter: %d\n", omp_get_thread_num(), i);
-	}
+	}*/
+
+	bucket_sort_init(argc,argv);
 }
 

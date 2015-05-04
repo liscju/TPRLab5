fccc47acafefcfad9056a0445d6f05e97a59825c
liscju <piotr.listkiewicz@gmail.com>
2015-05-04 12:54:54 +0200

    Working on sorting

--- liscju/main.cpp
+++ liscju/main.cpp
@@ -10,10 +10,10 @@ void usage(char *program_name);
 void generate_array();
 void divide_points_to_buckets();
 void initialize_buckets();
-
 void sort_points();
-
 void print_sorted_points();
+void initialize_output_array();
+void check_if_output_array_sorted();
 
 using namespace std;
 
@@ -70,9 +70,22 @@ void bucket_sort_init(int argc,char **argv) {
 	divide_points_to_buckets();
 	print_buckets();
 
-	sorted_array = new float[size_of_array];
+	initialize_output_array();
 	sort_points();
 	print_sorted_points();
+	check_if_output_array_sorted();
+}
+
+void check_if_output_array_sorted() {
+	for (int i = 1; i < size_of_array; ++i) {
+		if (sorted_array[i] < sorted_array[i - 1]) {
+			cout << "sorted_array[" << i << "] < sorted_array[" << i-1 << "]" << endl;
+		}
+	}
+}
+
+void initialize_output_array() {
+	sorted_array = new float[size_of_array];
 }
 
 void print_sorted_points() {
@@ -127,38 +140,5 @@ void initialize_buckets() {
 
 /* Driver program to test above funtion */
 int main(int argc,char **argv) {
-/*
-	omp_set_num_threads(3);
-	#pragma omp parallel for
-	for (int i=0;i<10;i++) {
-		printf("Thread id: %d iter: %d\n", omp_get_thread_num(), i);
-	}*/
-
 	bucket_sort_init(argc,argv);
 }
-
-
-// Function to sort arr[] of size n using bucket sort
-void bucketSort(float arr[], int n)
-{
-	// 1) Create n empty buckets
-	vector<float> b[n];
-
-	// 2) Put array elements in different buckets
-	for (int i=0; i<n; i++)
-	{
-		int bi = n*arr[i]; // Index in bucket
-		b[bi].push_back(arr[i]);
-	}
-
-	// 3) Sort individual buckets
-	for (int i=0; i<n; i++)
-		sort(b[i].begin(), b[i].end());
-
-	// 4) Concatenate all buckets into arr[]
-	int index = 0;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < b[i].size(); j++)
-			arr[index++] = b[i][j];
-}
-
